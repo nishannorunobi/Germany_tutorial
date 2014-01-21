f23050fb47d9fb4cfb3298c866c25b343bbad3f7
nishannorunnobi <nishan.cse17@yahoo.com>
2014-01-21 18:21:47 +0600

    new

--- src/german_tutorial_netbean/Suchen.java
+++ src/german_tutorial_netbean/Suchen.java
@@ -6,17 +6,33 @@
 
 package german_tutorial_netbean;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
  * @author nishan
  */
-public class German_tutorial_netbean {
-
+public class Suchen{
+    private static Set<String> verbindungen = new HashSet<String>();
+    private static Set<String> verbindungenT = new TreeSet<String>();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public void initializer(){
+        verbindungen.add("");
+        verbindungenT.ad
+    }
+    
+    private void Suche(){
+        
+        
     }
     
 }
